58dd556620f8a27c47b411b86a0b68a6a5281319
Jan <hofrrichter.jan1@gmail.com>
2023-08-28 14:52:49 +0200

    Adding priority design to indicate task level

--- client/src/Pages/ToDoItem.js
+++ client/src/Pages/ToDoItem.js
@@ -37,40 +37,53 @@ export function ToDoItem({
 
   return (
     <>
-      <div
-        className={
-          selecteIndex === index
-            ? "list-group-item list-group-item-primary clickable-button"
-            : "list-group-item clickable-button"
-        }
-        onClick={() => {
-          setSelectedIndex(index);
-          handleChange({
-            ID: _id,
-            name: name,
-            description: description,
-            date: date,
-            priority: priority,
-            completed: completed,
-          });
-        }}
-      >
-        <input
-          className="form-check-input me-1 left"
-          defaultChecked={completed}
-          onChange={(e) =>
-            updateTaskField(_id, e.target.checked, updateChecked)
+      <div className="inline-elements">
+        <div
+          className={
+            priority === "High"
+              ? "color-div-red"
+              : priority === "Mid"
+              ? "color-div-orange"
+              : priority === "Low"
+              ? "color-div-yellow"
+              : "color-div-grey"
           }
-          type="checkbox"
-        />
-        <label className={completed === true ? "crossed left" : "left"}>
-          {name}
-        </label>
-        <Button
-          label={"Delete"}
-          func={() => deleteTask(_id, deleteElem)}
-          buttonClass={"delete-button"}
-        />
+        ></div>
+        <div
+          className={
+            selecteIndex === index
+              ? "list-group-item list-group-item-primary clickable-button full-width"
+              : "list-group-item clickable-button full-width"
+          }
+          onClick={() => {
+            setSelectedIndex(index);
+            handleChange({
+              ID: _id,
+              name: name,
+              description: description,
+              date: date,
+              priority: priority,
+              completed: completed,
+            });
+          }}
+        >
+          <input
+            className="form-check-input me-1 left"
+            defaultChecked={completed}
+            onChange={(e) =>
+              updateTaskField(_id, e.target.checked, updateChecked)
+            }
+            type="checkbox"
+          />
+          <label className={completed === true ? "crossed left" : "left"}>
+            {name}
+          </label>
+          <Button
+            label={"Delete"}
+            func={() => deleteTask(_id, deleteElem)}
+            buttonClass={"delete-button"}
+          />
+        </div>
       </div>
     </>
   );
